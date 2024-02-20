941fd89f97f1c00069f1c2ba27c9ee7dae7b1ada
pinti0001 <pinti000pk@gmail.com>
2024-02-20 23:10:39 +0530

    TL

---
+++ js.js
@@ -0,0 +1,24 @@
+function colorr(color){
+    document.getElementById('rj').style.backgroundColor= 'red';
+    document.getElementById('rj').style.boxShadow= '3px 3px 30px 10px red';
+    document.getElementById('yj').style.backgroundColor= 'rgb(240, 240, 208)';
+    document.getElementById('yj').style.boxShadow= 'none';
+    document.getElementById('gj').style.backgroundColor= 'rgb(211, 239, 211)';
+    document.getElementById('gj').style.boxShadow= 'none ';
+}
+function colory(color){
+    document.getElementById('rj').style.backgroundColor= 'rgb(233, 190, 190)';
+    document.getElementById('rj').style.boxShadow= 'none';
+    document.getElementById('yj').style.backgroundColor= 'yellow';
+    document.getElementById('yj').style.boxShadow= '3px 3px 30px 15px yellow';
+    document.getElementById('gj').style.backgroundColor= 'rgb(211, 239, 211)';
+    document.getElementById('gj').style.boxShadow= 'none ';
+}
+function colorg(color){
+    document.getElementById('rj').style.backgroundColor= 'rgb(233, 190, 190)';
+    document.getElementById('rj').style.boxShadow= 'none';
+    document.getElementById('yj').style.backgroundColor= 'rgb(240, 240, 208)';
+    document.getElementById('yj').style.boxShadow= 'none';
+    document.getElementById('gj').style.backgroundColor= 'green';
+    document.getElementById('gj').style.boxShadow= '3px 3px 30px 10px green';
+}
